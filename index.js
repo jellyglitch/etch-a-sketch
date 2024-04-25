0269fac8105880ca141dc017ad6d4554654173db
Jane Tang <57682395+jellyglitch@users.noreply.github.com>
2024-04-25 17:01:21 -0500

    Make grid resizable with a button by taking user input.

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const container = document.querySelector(".container");
-const numOfSq = 16*16;
+let size = 16;
+let numOfSq = size*size;
 let count = 0;
 
-while(count < numOfSq){
-    const div = document.createElement("div");
-    div.classList.add("squares");
-    container.appendChild(div);
-    div.addEventListener("mousemove", (e) => {
-        div.classList.add("etch");
-    });
-    count++;
+createGrid(numOfSq, size);
+
+function createGrid(numOfSq, size){
+    let measurement = 960/size;
+    while(count < numOfSq){
+        const div = document.createElement("div");
+        div.style.width = measurement+'px';
+        div.style.height = measurement+'px';
+        container.appendChild(div);
+        div.addEventListener("mousemove", (e) => {
+            div.classList.add("etch");
+        });
+        count++;
+    }
 }
 
+function newGrid(){
+    let size = prompt("Enter how many squares you want your canvas to have per side.");
+    let validSize = false;
 
+    while (!validSize){
 
+        if(size > 0 && size < 100){
+            count = 0;
+            numOfSq = size*size;
+
+            const removeContainer = document.querySelector(".container");
+
+            while (removeContainer.firstChild){
+                removeContainer.removeChild(removeContainer.firstChild);
+            }
+
+            validSize = true;
+            createGrid(numOfSq, size);
+        } else {
+            size = prompt("Make sure you pick a number between 0-100.");
+        }
+    }
+    
+    
+}

ad25d2b930f669b5abc32399c54b79bb77d16b67
Jane Tang <57682395+jellyglitch@users.noreply.github.com>
2024-04-26 22:40:08 -0500

    Add darken color functionality. Colours darken every time you go over it until it turns black.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let size = 16;
 let numOfSq = size*size;
 let count = 0;
 
+let opacityChange = 0.1;
+
 createGrid(numOfSq, size);
 
 function createGrid(numOfSq, size){
@@ -13,11 +15,25 @@ function createGrid(numOfSq, size){
         div.style.height = measurement+'px';
         container.appendChild(div);
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        div.addEventListener("mousemove", (e) => {
+        div.addEventListener("mouseenter", (e) => {
             div.style.backgroundColor = "#" + randomColor;
+            let brightness = getBrightness(div);
+            let updateBrightness = brightness-0.1;
+            div.style.filter = `brightness(${updateBrightness})`;
         });
         count++;
     }
+    
+}
+
+function getBrightness(element){
+    const computedStyle = window.getComputedStyle(element);
+    const filterValue = computedStyle.getPropertyValue('filter');
+    const brightnessMatch = filterValue.match(/brightness\(([^)]+)\)/);
+    if (brightnessMatch) {
+        return parseFloat(brightnessMatch[1]);
+    }
+    return 1; 
 }
 
 function newGrid(){
